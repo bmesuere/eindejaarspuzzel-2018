1d8ac8ee6a56f16590cc5bf6b5420daddb409c7d
Jens Timmerman <jens.timmerman@gmail.com>
2019-01-14 08:04:29 +0100

    added viginere cryptanalysis code

---
+++ opgave23/viginerecrack.py
@@ -0,0 +1,90 @@
+import numpy
+
+
+NL_HIST = {'a':7, 'b':1, 'c':1, 'd':6, 'e': 19, 'f':1, 'g': 3, 'h': 2, 'i': 6, 'j': 1, 'k': 2, 'l': 4, 'm': 2, 'n':
+        10, 'o': 6, 'p': 1, 'q': 0, 'r': 6, 's': 4, 't': 7, 'u': 2, 'v': 3, 'w': 2, 'x': 0, 'y': 0, 'z': 1}
+
+def histogram(text):
+    """create a histogram of the given text"""
+    hist = {}
+    for i in text:
+        hist[i] = hist.pop(i, 0) + 1
+    return hist
+
+
+def analyzelanguage(languagestring):
+    """returns the histogram (in %) of characters used in the given language"""
+    # todo: use n-grams for more then 1 letter? in extra function
+    hist = histogram(languagestring)
+    total = len(languagestring)
+    for i, j in hist.iteritems():
+        hist[i] = j*100/total
+    return hist
+
+def dictcorrelation(dict1, dict2):
+    """Calculate the correlation between two dicts"""
+    keys = set(dict1.keys() + dict2.keys())
+    return numpy.corrcoef(
+        [dict1.get(x, 0) for x in keys],
+        [dict2.get(x, 0) for x in keys])[0, 1]
+
+def scorelanguage(languagestring, language_hist=None):
+    """
+    give a score of how well the given input matches the histogram of characters used in that language (between 0 and 1)
+    this works on indiviual letters, so you can give slices without worrying about ngrams
+    """
+    if not language_hist:
+        language_hist = ENG_HIST.copy()
+    hist = histogram(languagestring)
+
+    # calculate the correlation
+    return dictcorrelation(language_hist, hist)
+
+def rot(a, b):
+    """rotate string a with letters of string b resp"""
+    c = ''
+    for i in range(0,min(len(a),len(b))):
+        c = c + chr((ord(a[i]) - 2*ord('a') + ord(b[i])) % 26 + ord('a'))
+    return c
+
+
+
+def crack(inputstring, decrypt_cipher, language_hist=ENG_HIST, confidence=.5, pw_length=1):
+    """
+    Crack the key and return it and the plaintext for cipher given a
+    We start with one char pw's only
+    """
+    bestscore = 0
+    bestkey = ''
+    bestenglish = ''
+    #for key in itertools.combinations_with_replacement(ascii_lowercase, pw_length):
+    for letter in string.ascii_letters:
+        key = letter * len(inputstring)
+        try:
+            # do the xorhex for a long string of char, decode it to hex to score our language
+            decrypted = decrypt_cipher(inputstring, "".join(key))
+        except TypeError:
+            # result wasn't hex, so lets skip
+            continue
+        english = scorelanguage(decrypted, language_hist)
+        if english > bestscore:
+            bestscore = english
+            bestkey = key
+            bestenglish = decrypted
+            print bestscore, bestkey, bestenglish
+        if english > confidence:
+            yield "".join(key), decrypted, english
+
+
+# it's cbc so we can crack 1 char at a time
+def cbccrack(blocklen, decrypt, inputstring, lang_hist, confidence=0.7):
+    for i in range(blocklen):
+        print 'next block'
+        inp = inputstring[i::blocklen]
+        for guess in crack(inp, decrypt, lang_hist, confidence,len(inputstring)//blocklen):
+            print(i, guess)
+
+# no good on short texts
+#cbccrack(9, rot, """OTNMFRSVGNLTWIQQJLDJASUBWNVARPEKVAABQGTINKAAJOBXSGRQAMYTAAJPKAKMGRQBBIHXQGJMXXGRZAOIYIMQCWJAKFWEYVIINYAWNFCMOMUHPTLRXFPBLYUZQEZTFNPQGCKDCXKNKVGLQZNHSCNMD""", lang_hist=NL_HIST, confidence=0.5)
+
+#TODO: dictinonary attack instead of cbc cryptanalyse
